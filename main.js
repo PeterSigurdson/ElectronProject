0370bf03c53523c25d43b7213a2dd1ce47b7b593
peter sigurdson <peter@petersigurdson.net>
2019-07-18 16:51:04 -0400

    5th  commit

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ const { app, BrowserWindow} = require('electron');
 
 const path = require('path');
 const url = require('url');
-let win
+let win;
 function createWindow(){
 	win = new BrowswerWindow({width:800, height:600})	
-}
-win.loadURL(url.format) ({
-	pathname:path.join(__dirname, 'main.html'), 
-	protocol:'file:'
+
+    win.loadURL(url.format) ({
+	pathname:path.join(__dirname, 'index.html'), 
+	protocol:'file:',
 	slashes: true
-})
-win.on('closed', () =? {
+});
+win.on('closed', () => {
 	win=null
 })
 
